b3b81f458f821921c76296df9e4aa469a761c957
Akash Keote <keoteakash@gmail.com>
2024-06-03 18:38:34 +0530

    Update Lists.jsx

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -176,7 +176,13 @@ const contributors = [
     role: "Contributor",
     github: "https://github.com/memickeymac03",
     image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
-  }
+  },
+   {
+    name: "Akash Keote",
+    role: "Contributor",
+    github: "https://github.com/AkashKeote",
+    image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
+  },
 ];
 
 export default function Lists() {
