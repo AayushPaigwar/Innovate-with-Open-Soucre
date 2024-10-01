6131908cb7f23a42de28f6e97a2fe80ad46bb326
NikhileshM24 <118903685+Nikhileshmauje@users.noreply.github.com>
2024-10-01 21:51:27 +0530

    Update contributors.js
    Added myself as a contributer in the contributer.js file

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -220,7 +220,12 @@ const contributors = [
       image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
     }
 
-    //add your code here//
+    {
+      name: "Nikhilesh Mauje",
+      role: "Contributor",
+      github: "https://github.com/Nikhileshmauje",
+      image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png" // or your image URL
+}
   ];
 
 export default contributors;
