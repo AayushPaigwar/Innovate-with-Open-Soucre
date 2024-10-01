56ce109157fd5734a337fd95b954dba7b925d885
Atharva Werulkar <werulkaratharva@gmail.com>
2024-10-01 20:55:29 +0530

    Registered myself as a contributor (#57)
    Co-authored-by: Aayush Paigwar <90213184+AayushPaigwar@users.noreply.github.com>

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -190,7 +190,12 @@ const contributors = [
     image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
   },
   {
-
+    name: "Athrva Werulkar",
+    role: "Contributor",
+    github: "https://github.com/Atharva-Werulkar",
+    image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
+  },
+  {
     name: "Priyadarshan Ghosh Hazra",
     role: "Contributor",
     github: "https://github.com/Priyadarshan-coder",
@@ -216,6 +221,7 @@ const contributors = [
     github:"https://github.com/KUNALCHOURE",
     image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
   }
+
 
 
 
