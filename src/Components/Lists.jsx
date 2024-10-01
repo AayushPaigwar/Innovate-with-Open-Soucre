379e772110ebe2d1dc73eb4da51cf817ec1d1336
KUNAL CHOURE <147177836+KUNALCHOURE@users.noreply.github.com>
2024-10-01 20:49:17 +0530

    added (#59)

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -210,6 +210,13 @@ const contributors = [
     image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png" // or your image URL
   },
 
+  {
+    name:"Kunal choure",
+    role:"Contributor",
+    github:"https://github.com/KUNALCHOURE",
+    image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png"
+  }
+
 
 
   /*=================ADD YOUR CODE ABOVE THIS================= */
