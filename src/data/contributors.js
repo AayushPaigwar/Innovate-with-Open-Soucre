4dc8e3cfae040fbe830a0dfda37ae47af7f49521
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2024-10-02 01:12:21 +0530

    Update contributors.js

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -242,6 +242,12 @@ const contributors = [
       github: "https://github.com/Gamerking177",
       image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png" // or your image URL
     },
+    {
+      name: "Vijayalaxmi Wakode",
+      role: "Contributor",
+      github: "https://github.com/vijayalaxmi777",
+      image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png" // or your image URL
+    },
   ];
 
 export default contributors;
