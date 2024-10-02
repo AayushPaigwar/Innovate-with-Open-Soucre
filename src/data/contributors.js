a3a4df5fbed001930d56ded32284282f3ee5d2b9
Nikhil Nandanwar <135251709+nikhilnandanwar429@users.noreply.github.com>
2024-10-02 09:58:19 +0530

    Update contributors.js

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -7,6 +7,12 @@ const contributors = [
     github: "https://github.com/Ketanop321",
     image: "https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_1280.png" // or your image URL
   },
+    {
+  name: "Nikhil Nandanwar",
+  role: "Contributor",
+  github: "https://github.com/nikhilnandanwar429",
+  image: "https://github.com/nikhilnandanwar429.png" 
+},
   
   
   
