00a8535536ab58b24012835af6f25fe9771435e1
Atharva Werulkar <werulkaratharva@gmail.com>
2024-10-02 12:50:27 +0530

    changed height width

--- components/cards.jsx
+++ components/cards.jsx
@@ -12,7 +12,7 @@ const FeatureCard = ({ name, role, image, github }) => (
         <div className="absolute inset-0 bg-grid-gray-100 opacity-50"></div>
         <div className="relative flex flex-col justify-between h-full z-10">
             <div className="flex justify-between items-start mb-4">
-                <div className="h-10 w-10">
+                <div className="h-20 w-20">
                     <img src={image} alt={name} className="h-10 w-10 rounded-full" />
                 </div>
             </div>
